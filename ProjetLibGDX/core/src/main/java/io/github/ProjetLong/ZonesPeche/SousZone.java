444a8b423a39e561bf91a28899e15507e9e740a0
Frenox <maxime.huet15@gmail.com>
2025-04-07 19:38:38 +0200

    feat: ajout classe poisson + getRandPoisson dans SousZone

--- ProjetLibGDX/core/src/main/java/io/github/ProjetLong/ZonesPeche/SousZone.java
+++ ProjetLibGDX/core/src/main/java/io/github/ProjetLong/ZonesPeche/SousZone.java
@@ -1,5 +1,6 @@
 package main.java.io.github.ProjetLong.ZonesPeche;
 import java.util.Map;
+import java.util.Random;
 import java.util.HashMap;
 
 public abstract class SousZone {
@@ -11,9 +12,27 @@ public abstract class SousZone {
         this.estDebloquee = estDebloquee;
     }
 
+    /**Renvoie un poisson aleatoire dans la liste de poissons disponibles
+     * en ponderant par les probabilites de chaque poisson
+     */
     public Poisson getRandPoisson() {
+        float probaTotale = 0.0f;
+        for (float probaPoisson : poissonsDispo.values()) {
+            probaTotale += probaPoisson;
+        }
+
+        float randomVal = new Random().nextFloat() * probaTotale;
+        float probaCumul = 0.0f;
+
+        for (Map.Entry<Poisson, Float> entry : poissonsDispo.entrySet()) {
+            probaCumul += entry.getValue();
+            if (randomVal <= probaCumul) {
+                return entry.getKey();
+            }
+        }
         return null;
     }
+
     public boolean getEstDebloquee() {
         return this.estDebloquee;
     }
