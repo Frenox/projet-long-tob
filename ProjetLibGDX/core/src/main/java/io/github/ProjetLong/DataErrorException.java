17a08d24807e724aacbee1bf684de96d0318e296
Frenox <maxime.huet15@gmail.com>
2025-05-05 15:26:40 +0200

    feat: ajout save/load + variables dataManager
    Mise en place du systeme de save/load via serialisation (bateaux a faire) et des variables de stockage des données (bateaux, stockagePoissons, argent)

---
+++ ProjetLibGDX/core/src/main/java/io/github/ProjetLong/DataErrorException.java
@@ -0,0 +1,11 @@
+package io.github.ProjetLong;
+
+public class DataErrorException extends Exception {
+    public DataErrorException() {
+        super();
+    }
+
+    public DataErrorException(String message) {
+        super(message);
+    }
+}
