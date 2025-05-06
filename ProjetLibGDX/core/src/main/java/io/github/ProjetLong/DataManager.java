5bcfd1b2b8dff1f55d045e12625f27ee39c7baf1
Mokoko <cocosurier@gmail.com>
2025-05-06 20:09:44 +0200

    cczcz

--- ProjetLibGDX/core/src/main/java/io/github/ProjetLong/DataManager.java
+++ ProjetLibGDX/core/src/main/java/io/github/ProjetLong/DataManager.java
@@ -18,9 +18,8 @@ import io.github.ProjetLong.Bateau;
 import io.github.ProjetLong.DataErrorException;
 import io.github.ProjetLong.ZonesPeche.Poisson;
 
-
 public class DataManager {
-    
+
     private List<Bateau> bateaux;
     private List<Poisson> stockagePoissons;
     private int argent;
@@ -33,11 +32,12 @@ public class DataManager {
         stockagePoissons = new ArrayList<Poisson>();
         argent = 0;
 
-        //...
+        // ...
     }
 
     /**
      * Charge le slot de jeu donne (avec ses donnees)
+     * 
      * @param slot (String) : Slot de sauvegarde a charger
      */
     public DataManager(String slot) {
@@ -45,19 +45,19 @@ public class DataManager {
         stockagePoissons = new ArrayList<Poisson>();
         argent = 0;
 
-        //...
+        // ...
 
         loadGame(slot);
     }
 
-    
-
     public List<Bateau> getBateaux() {
         return bateaux;
     }
+
     public void ajouterBateauPort(Bateau bateau) {
         bateaux.add(bateau);
     }
+
     public void supprimerBateauPort(Bateau bateau) {
         bateaux.remove(bateau);
     }
@@ -65,9 +65,11 @@ public class DataManager {
     public List<Poisson> getStockage() {
         return stockagePoissons;
     }
+
     public void ajouterPoissonStockage(Poisson poisson) {
         stockagePoissons.add(poisson);
     }
+
     public void supprimerPoissonStockage(Poisson poisson) {
         stockagePoissons.remove(poisson);
     }
@@ -75,12 +77,14 @@ public class DataManager {
     public int getArgent() {
         return argent;
     }
+
     public void ajouterArgent(int montant) {
         if (montant <= 0) {
             return;
         }
         argent += montant;
     }
+
     public void retirerArgent(int montant) {
         if (montant <= 0) {
             return;
@@ -88,29 +92,24 @@ public class DataManager {
         argent -= montant;
     }
 
-
-
-
-
-
-
     /**
-     * Serialise et sauvegarde les donnees du jeu au format JSON 
+     * Serialise et sauvegarde les donnees du jeu au format JSON
      * dans le fichier du slot de jeu actuel
+     * 
      * @param slot (String) : Slot de sauvegarde a charger
      * @return ______ (bool) : La sauvegarde a-t-elle reussi
      */
     public boolean saveGame(String slot) {
         Map<String, Object> data = new HashMap<>();
-        
+
         try {
-            //Ajout des donnees a sauvegarder
+            // Ajout des donnees a sauvegarder
             data.put("Bateaux", serializeBateaux());
             data.put("StockagePoissons", serializePoissons());
             data.put("Argent", serializeArgent());
 
             ObjectMapper mapper = new ObjectMapper();
-            //Ecriture des donnees
+            // Ecriture des donnees
             mapper.writerWithDefaultPrettyPrinter().writeValue(new File(slot + ".json"), data);
 
             System.out.println("Jeu sauvegarde dans " + slot + ".json");
@@ -121,9 +120,11 @@ public class DataManager {
 
         return true;
     }
+
     /**
-     * Deserialise et charge les donnees du jeu au format JSON 
+     * Deserialise et charge les donnees du jeu au format JSON
      * depuis le fichier du slot de jeu selectionne
+     * 
      * @param slot (String) : Slot de sauvegarde a charger
      * @return ______ (bool) : Le chargement a-t-il reussi
      */
@@ -131,17 +132,16 @@ public class DataManager {
         Map<String, String> data = new HashMap<>();
 
         try {
-            //Ouverture du fichier
+            // Ouverture du fichier
             File dataFile = new File(slot + ".json");
-            
+
             ObjectMapper mapper = new ObjectMapper();
-            //Lecture des donnees
+            // Lecture des donnees
             data = mapper.readValue(dataFile, Map.class);
 
             deserializeBateaux(data.get("Bateaux"));
             deserializePoissons(data.get("StockagePoissons"));
             deserializeArgent(data.get("Argent"));
-
 
             System.out.println("Jeu charge depuis " + slot + ".json");
         } catch (Exception e) {
@@ -152,12 +152,10 @@ public class DataManager {
         return true;
     }
 
-
-
-
     /**
-     * Serialise et concatene les donnees des bateaux en 
+     * Serialise et concatene les donnees des bateaux en
      * les separant par le caractere "|"
+     * 
      * @return ______ (String) : Chaine de caracteres serialisee des bateaux
      */
     private String serializeBateaux() {
@@ -168,8 +166,10 @@ public class DataManager {
         }
         return serializedData;
     }
+
     /**
      * Deserialise et les donnees des bateaux separees par le caractere "|"
+     * 
      * @param serializedData (String) : Chaine de caracteres serialisee des bateaux
      */
     private void deserializeBateaux(String serializedData) throws DataErrorException {
@@ -181,7 +181,7 @@ public class DataManager {
             String[] splitData = serializedData.split("\\|");
             for (String data : splitData) {
                 // Deserialisation du poisson
-                
+
             }
         } catch (Exception e) {
             System.out.println("Echec du chargement des bateaux.");
@@ -190,8 +190,9 @@ public class DataManager {
     }
 
     /**
-     * Serialise et concatene les donnees des poissons en 
+     * Serialise et concatene les donnees des poissons en
      * les separant par le caractere "|"
+     * 
      * @return ______ (String) : Chaine de caracteres serialisee des poissons
      */
     private String serializePoissons() {
@@ -202,8 +203,10 @@ public class DataManager {
         }
         return serializedData;
     }
+
     /**
      * Deserialise et les donnees des poissons separees par le caractere "|"
+     * 
      * @param serializedData (String) : Chaine de caracteres serialisee des poissons
      */
     private void deserializePoissons(String serializedData) throws DataErrorException {
@@ -226,13 +229,16 @@ public class DataManager {
 
     /**
      * Serialise l'argent
+     * 
      * @return ______ (String) : Chaine de caracteres serialisee de l'argent
      */
     private String serializeArgent() {
         return String.valueOf(argent);
     }
+
     /**
      * Deserialise et les donnees de l'argent
+     * 
      * @param serializedData (String) : Chaine de caracteres serialisee de l'argent
      */
     private void deserializeArgent(String serializedData) throws DataErrorException {
