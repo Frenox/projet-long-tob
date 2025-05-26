b7f15553ef737c2308edaf66ab516fcd65a54a2a
Mokoko <cocosurier@gmail.com>
2025-05-26 22:09:18 +0200

    rtgesddsgdsgdsgdsgdsg

--- ProjetLibGDX/core/src/main/java/io/github/ProjetLong/BatimentMarket.java
+++ ProjetLibGDX/core/src/main/java/io/github/ProjetLong/BatimentMarket.java
@@ -14,23 +14,23 @@ import io.github.ProjetLong.ZonesPeche.Poisson;
 
 public class BatimentMarket implements Batiment {
 
-    //etat d'ouverture du batiment
+    // etat d'ouverture du batiment
     private boolean isOpened;
-    //etat de vente ou non
+    // etat de vente ou non
     private boolean isSelling;
-    //argent joueur
+    // argent joueur
     private int coinAmount;
-    //poisson selectionné en vente
+    // poisson selectionné en vente
     private int selectedfish;
     private int offsetSelectedFish;
-    //page de poisson actuelle
+    // page de poisson actuelle
     private int page;
-    //position curseur
+    // position curseur
     private Vector3 mouse;
 
     private Stage stage;
 
-    //texture et sprite pour tout le batiment
+    // texture et sprite pour tout le batiment
     private Texture fishInv;
     private Texture batMarketTexture;
     private Sprite batMarketSprite;
@@ -45,26 +45,26 @@ public class BatimentMarket implements Batiment {
     private Sprite flecheGaucheSprite;
     private Sprite flecheDroiteSprite;
 
-    //liste des poissons du marché
+    // liste des poissons du marché
     private List<Poisson> poissons;
 
-    //son
+    // son
     private static final Sound cashSfx = Gdx.audio.newSound(Gdx.files.internal("audio/CashSound.mp3"));
 
     public BatimentMarket(DataManager data) {
 
-        //batiment ferme et pas en vente
+        // batiment ferme et pas en vente
         this.isOpened = false;
         this.isSelling = false;
 
-        //ecran du port
+        // ecran du port
         this.stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
-        //initialisation
+        // initialisation
         this.mouse = new Vector3(0, 0, 0);
 
-        //texture
+        // texture
         this.batMarketTexture = new Texture("bat1.png");
         this.interfaceBatMarketTexture = new Texture("overlayMarche.png");
         this.validerVenteTexture = new Texture("valider.png");
@@ -73,7 +73,7 @@ public class BatimentMarket implements Batiment {
         this.flecheDroiteTexture = new Texture("minigame2_arrow_right_fishing.png");
         this.fishInv = new Texture("fish_tab_fish.png");
 
-        //sprite
+        // sprite
         this.batMarketSprite = new Sprite(batMarketTexture);
         this.interfaceOverlayBatMarketSprite = new Sprite(interfaceBatMarketTexture);
         this.validerVenteTextureSprite = new Sprite(validerVenteTexture);
@@ -81,7 +81,7 @@ public class BatimentMarket implements Batiment {
         this.flecheGaucheSprite = new Sprite(flecheGaucheTexture);
         this.flecheDroiteSprite = new Sprite(flecheDroiteTexture);
 
-        //position initiale des sprites
+        // position initiale des sprites
         this.interfaceOverlayBatMarketSprite.setPosition(130, 50);
         this.validerVenteTextureSprite.setPosition(280, 90);
         this.refuserVenteTextureSprite.setPosition(335, 90);
@@ -90,19 +90,19 @@ public class BatimentMarket implements Batiment {
         this.flecheGaucheSprite.setSize(15, 11);
         this.flecheDroiteSprite.setSize(15, 11);
 
-        //initialisation de plusieurs variables
+        // initialisation de plusieurs variables
         this.page = 0;
         this.selectedfish = 0;
         this.offsetSelectedFish = 0;
-        
-        //liste des poissons 
+
+        // liste des poissons
         this.poissons = data.getStockage();
 
     }
 
     @Override
     public void input(VilleScreen screen) {
-        //recupere les coordonnees du curseur si clic gauche
+        // recupere les coordonnees du curseur si clic gauche
         if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
             this.mouse = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
             screen.jeu.viewport.getCamera().unproject(mouse);
@@ -113,11 +113,11 @@ public class BatimentMarket implements Batiment {
 
     @Override
     public void logic(VilleScreen screen) {
-        //recupere les poissons et l'argent du joueur
+        // recupere les poissons et l'argent du joueur
         this.poissons = screen.jeu.data.getStockage();
         this.coinAmount = screen.jeu.data.getArgent();
 
-        //si ouvert mais pas en vente
+        // si ouvert mais pas en vente
         if (this.isOpened && this.isSelling == false) {
             // changer de page
             if (mouse.x >= 215 && mouse.x <= 230 && mouse.y >= 56 && mouse.y <= 67
@@ -128,7 +128,7 @@ public class BatimentMarket implements Batiment {
                 page--;
             }
 
-            //detection de la vente (identifiez quel poisson)
+            // detection de la vente (identifiez quel poisson)
             if (mouse.x >= 140 && mouse.x <= 265 && mouse.y >= 70 && mouse.y < 200) {
                 while (mouse.y > (70 + offsetSelectedFish * 18.75)) {
                     offsetSelectedFish++;
@@ -144,24 +144,24 @@ public class BatimentMarket implements Batiment {
             }
         }
 
-        //si ouvert et en vente
+        // si ouvert et en vente
         if (this.isOpened && this.isSelling == true) {
-            //validation vente
+            // validation vente
             if (mouse.x > 280 && mouse.x < 310 && mouse.y > 90 && mouse.y < 120) {
-                //on crédite le joueur
+                // on crédite le joueur
                 screen.jeu.data.ajouterArgent(this.poissons.get(selectedfish).getPrix());
-                //son vente
+                // son vente
                 cashSfx.play();
 
                 // on enlève le poisson dans la session ET dans le data manager
                 this.poissons.remove(selectedfish);
 
-                //reset des variables
+                // reset des variables
                 this.isSelling = false;
                 this.selectedfish = 0;
                 this.offsetSelectedFish = 0;
             }
-            //refus vente
+            // refus vente
             if (mouse.x > 335 && mouse.x < 365 && mouse.y > 90 && mouse.y < 120) {
                 this.isSelling = false;
                 this.selectedfish = 0;
@@ -172,7 +172,7 @@ public class BatimentMarket implements Batiment {
 
     @Override
     public void draw(VilleScreen screen, int position, int offset) {
-        //dessine le batiment
+        // dessine le batiment
         this.batMarketSprite.setPosition(position * 64 + offset, 91);
         this.batMarketSprite.draw(screen.jeu.batch);
         screen.jeu.HebertBold.draw(screen.jeu.batch, "Market", 64 * position + offset, 180);
@@ -254,14 +254,14 @@ public class BatimentMarket implements Batiment {
         this.isOpened = value;
     }
 
-    //fermeture interface
+    // fermeture interface
     public void close() {
         this.isSelling = false;
         this.selectedfish = 0;
         this.offsetSelectedFish = 0;
     }
 
-    //ouverture interface
+    // ouverture interface
     public void open() {
         System.out.println("ouverture market");
     }
