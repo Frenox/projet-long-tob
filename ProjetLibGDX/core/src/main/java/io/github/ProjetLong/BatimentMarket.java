b0df3d672414649a60ba02fb3132a4a040a93fc9
Martin <cornillad.martin@yahoo.com>
2025-05-26 19:28:32 +0200

    commentaires principalement

--- ProjetLibGDX/core/src/main/java/io/github/ProjetLong/BatimentMarket.java
+++ ProjetLibGDX/core/src/main/java/io/github/ProjetLong/BatimentMarket.java
@@ -1,8 +1,6 @@
 package io.github.ProjetLong;
 
-import java.util.ArrayList;
 import java.util.List;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Sound;
@@ -11,100 +9,100 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-
 import io.github.ProjetLong.DataManager.DataManager;
 import io.github.ProjetLong.ZonesPeche.Poisson;
 
 public class BatimentMarket implements Batiment {
 
+    //etat d'ouverture du batiment
     private boolean isOpened;
+    //etat de vente ou non
     private boolean isSelling;
-
-    public static int coinAmount; // modifié pour pouvoir l'utiliser
+    //argent joueur
+    private int coinAmount;
+    //poisson selectionné en vente
     private int selectedfish;
     private int offsetSelectedFish;
-
+    //page de poisson actuelle
     private int page;
+    //position curseur
     private Vector3 mouse;
 
     private Stage stage;
 
+    //texture et sprite pour tout le batiment
     private Texture fishInv;
-
     private Texture batMarketTexture;
     private Sprite batMarketSprite;
-
     private Texture interfaceBatMarketTexture;
     private Sprite interfaceOverlayBatMarketSprite;
-
     private Texture validerVenteTexture;
     private Texture refuserVenteTexture;
-
     private Sprite validerVenteTextureSprite;
     private Sprite refuserVenteTextureSprite;
-
     private Texture flecheGaucheTexture;
     private Texture flecheDroiteTexture;
     private Sprite flecheGaucheSprite;
     private Sprite flecheDroiteSprite;
 
-    // a supprimer quand implémente
+    //liste des poissons du marché
     private List<Poisson> poissons;
 
+    //son
     private static final Sound cashSfx = Gdx.audio.newSound(Gdx.files.internal("audio/CashSound.mp3"));
 
-    // 512 par 288
-
     public BatimentMarket(DataManager data) {
+
+        //batiment ferme et pas en vente
         this.isOpened = false;
         this.isSelling = false;
+
+        //ecran du port
         this.stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
+        //initialisation
         this.mouse = new Vector3(0, 0, 0);
 
+        //texture
         this.batMarketTexture = new Texture("bat1.png");
         this.interfaceBatMarketTexture = new Texture("overlayMarche.png");
         this.validerVenteTexture = new Texture("valider.png");
         this.refuserVenteTexture = new Texture("refuser.png");
+        this.flecheGaucheTexture = new Texture("minigame2_arrow_left_fishing.png");
+        this.flecheDroiteTexture = new Texture("minigame2_arrow_right_fishing.png");
+        this.fishInv = new Texture("fish_tab_fish.png");
 
+        //sprite
         this.batMarketSprite = new Sprite(batMarketTexture);
         this.interfaceOverlayBatMarketSprite = new Sprite(interfaceBatMarketTexture);
         this.validerVenteTextureSprite = new Sprite(validerVenteTexture);
         this.refuserVenteTextureSprite = new Sprite(refuserVenteTexture);
-
-        // this.batMarketSprite.setPosition(0, 90);
-        this.interfaceOverlayBatMarketSprite.setPosition(130, 50);
-        this.validerVenteTextureSprite.setPosition(280, 90);
-        this.refuserVenteTextureSprite.setPosition(335, 90);
-
-        this.page = 0;
-        this.selectedfish = 0;
-        this.offsetSelectedFish = 0;
-        this.fishInv = new Texture("fish_tab_fish.png");
-
-        this.poissons = data.getStockage();
-        this.flecheGaucheTexture = new Texture("minigame2_arrow_left_fishing.png");
-        this.flecheDroiteTexture = new Texture("minigame2_arrow_right_fishing.png");
-
         this.flecheGaucheSprite = new Sprite(flecheGaucheTexture);
         this.flecheDroiteSprite = new Sprite(flecheDroiteTexture);
 
+        //position initiale des sprites
+        this.interfaceOverlayBatMarketSprite.setPosition(130, 50);
+        this.validerVenteTextureSprite.setPosition(280, 90);
+        this.refuserVenteTextureSprite.setPosition(335, 90);
         this.flecheGaucheSprite.setPosition(175, 56); // Flèche gauche
         this.flecheDroiteSprite.setPosition(215, 56); // Flèche droite
         this.flecheGaucheSprite.setSize(15, 11);
         this.flecheDroiteSprite.setSize(15, 11);
 
-        // a supprimer later
-        this.poissons = new ArrayList<>();
-        for (int i = 0; i <= 100; i++) {
-            this.poissons.add(new Poisson(1, 1));
-        }
+        //initialisation de plusieurs variables
+        this.page = 0;
+        this.selectedfish = 0;
+        this.offsetSelectedFish = 0;
+        
+        //liste des poissons 
+        this.poissons = data.getStockage();
 
     }
 
     @Override
     public void input(VilleScreen screen) {
+        //recupere les coordonnees du curseur si clic gauche
         if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
             this.mouse = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
             screen.jeu.viewport.getCamera().unproject(mouse);
@@ -115,9 +113,11 @@ public class BatimentMarket implements Batiment {
 
     @Override
     public void logic(VilleScreen screen) {
+        //recupere les poissons et l'argent du joueur
         this.poissons = screen.jeu.data.getStockage();
         this.coinAmount = screen.jeu.data.getArgent();
 
+        //si ouvert mais pas en vente
         if (this.isOpened && this.isSelling == false) {
             // changer de page
             if (mouse.x >= 215 && mouse.x <= 230 && mouse.y >= 56 && mouse.y <= 67
@@ -128,7 +128,7 @@ public class BatimentMarket implements Batiment {
                 page--;
             }
 
-            // detection de la vente (identifiez quel poisson)
+            //detection de la vente (identifiez quel poisson)
             if (mouse.x >= 140 && mouse.x <= 265 && mouse.y >= 70 && mouse.y < 200) {
                 while (mouse.y > (70 + offsetSelectedFish * 18.75)) {
                     offsetSelectedFish++;
@@ -144,19 +144,24 @@ public class BatimentMarket implements Batiment {
             }
         }
 
+        //si ouvert et en vente
         if (this.isOpened && this.isSelling == true) {
+            //validation vente
             if (mouse.x > 280 && mouse.x < 310 && mouse.y > 90 && mouse.y < 120) {
-                // on crédite le joueur
+                //on crédite le joueur
                 screen.jeu.data.ajouterArgent(this.poissons.get(selectedfish).getPrix());
+                //son vente
                 cashSfx.play();
 
                 // on enlève le poisson dans la session ET dans le data manager
                 this.poissons.remove(selectedfish);
 
+                //reset des variables
                 this.isSelling = false;
                 this.selectedfish = 0;
                 this.offsetSelectedFish = 0;
             }
+            //refus vente
             if (mouse.x > 335 && mouse.x < 365 && mouse.y > 90 && mouse.y < 120) {
                 this.isSelling = false;
                 this.selectedfish = 0;
@@ -167,7 +172,7 @@ public class BatimentMarket implements Batiment {
 
     @Override
     public void draw(VilleScreen screen, int position, int offset) {
-        // always draw the batiment itself
+        //dessine le batiment
         this.batMarketSprite.setPosition(position * 64 + offset, 91);
         this.batMarketSprite.draw(screen.jeu.batch);
         screen.jeu.HebertBold.draw(screen.jeu.batch, "Market", 64 * position + offset, 180);
@@ -184,7 +189,7 @@ public class BatimentMarket implements Batiment {
             // Affichage des coins
             // Mis en jaune, puis re en blanc
             screen.jeu.HebertBold.setColor(1, 1, 0, 1); // couleur jaune
-            screen.jeu.HebertBold.draw(screen.jeu.batch, "COINS : " + BatimentMarket.coinAmount, 300, 240);
+            screen.jeu.HebertBold.draw(screen.jeu.batch, "COINS : " + this.coinAmount, 300, 240);
             screen.jeu.HebertBold.setColor(1, 1, 1, 1);
 
             // Affichage des instructions
@@ -249,12 +254,14 @@ public class BatimentMarket implements Batiment {
         this.isOpened = value;
     }
 
+    //fermeture interface
     public void close() {
         this.isSelling = false;
         this.selectedfish = 0;
         this.offsetSelectedFish = 0;
     }
 
+    //ouverture interface
     public void open() {
         System.out.println("ouverture market");
     }
--- ProjetLibGDX/core/src/main/java/io/github/ProjetLong/Minijeu2.java
+++ ProjetLibGDX/core/src/main/java/io/github/ProjetLong/Minijeu2.java
@@ -1,21 +1,16 @@
 package io.github.ProjetLong;
 
+//import
 import java.util.Random;
-
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class Minijeu2 implements Minijeu {
 
-    private Skin skin;
     private Stage stage;
 
     private Texture fish;
@@ -27,50 +22,61 @@ public class Minijeu2 implements Minijeu {
     private int directionX;
     private int directionY;
     private int cooldown = 60;
+    private final static double proba = 0.982;
     private int State; // 0 si inactif, 1 si actif, 2 si finit
 
     public Minijeu2() {
-        skin = new Skin(Gdx.files.internal("metalui/metal-ui.json"));
+        //stage
         stage = new Stage(new ScreenViewport());
-
         Gdx.input.setInputProcessor(stage);
 
+        //chargement des textures
         this.fish = new Texture("poisson.png");
         this.bubble = new Texture("CercleMinijeu3.png");
 
+        //mise en place des sprites
         this.fishSprite = new Sprite(fish);
         this.bubbleSprite = new Sprite(bubble);
+
+        //generateur random
         Random r = new Random();
+    
+        //initialisation de la position de depart du poisson et de la bulle
         fishSprite.setPosition(r.nextInt(100) + 50, 100);
         bubbleSprite.setPosition(Gdx.input.getX(), Gdx.input.getY());
+
+        //definit le sens de deplacement sur x initial de manière aléatoire
         switch (r.nextInt(2)) {
             case 1:
                 this.directionX = 1;
                 break;
-
             default:
                 this.directionX = -1;
                 fishSprite.flip(true, false);
                 break;
         }
 
+        //definit l'état "en fonctionnement" et le déplacement sur Y de départ
         this.directionY = 0;
-
         this.State = 1;
 
     }
 
+    //gère les inputs pour le mini-jeu
     @Override
     public void input(PecheActiveScreen screen) {
+        //recupère les coordonnées du curseur
         Vector3 mouse = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
         screen.jeu.viewport.getCamera().unproject(mouse);
 
+        //déplace la bulle à la position du curseur
         bubbleSprite.setPosition(
                 mouse.x - bubbleSprite.getWidth() / 2f,
                 mouse.y - bubbleSprite.getHeight() / 2f);
 
     }
 
+    //effectue la logique du mini-jeu
     @Override
     public void logic(PecheActiveScreen screen) {
         float x_limit_min = 50;
@@ -80,31 +86,36 @@ public class Minijeu2 implements Minijeu {
         float speed = 0.75f;
         float speedy = 0.40f;
 
-        // Déplacement du poisson sur l'axe X
+        //déplacement du poisson sur l'axe X
         fishSprite.translateX(speed * this.directionX);
         if (fishSprite.getX() < x_limit_min || fishSprite.getX() > x_limit_max) {
             this.directionX *= -1;
             fishSprite.flip(true, false);
+        
+        //changement de direction potentiel si cooldown < 0
+        //augmenter proba si on veut changer de direction moins souvent
         } else {
-            if (new Random().nextFloat() > 0.982 && cooldown < 0) {
+            if (new Random().nextFloat() > Minijeu2.proba && cooldown < 0) {
                 this.directionX *= -1;
                 fishSprite.flip(true, false);
                 cooldown = 45;
             }
         }
+
         cooldown -= 1;
+
         // calculer les centres du poisson et de la bulle
         float fishCenterX = fishSprite.getX() + fishSprite.getWidth() / 2f;
         float fishCenterY = fishSprite.getY() + fishSprite.getHeight() / 2f;
         float bubbleCenterX = bubbleSprite.getX() + bubbleSprite.getWidth() / 2f;
         float bubbleCenterY = bubbleSprite.getY() + bubbleSprite.getHeight() / 2f;
 
-        // distance entre les centres du poisson et de la bulle
+        //distance entre les centres du poisson et de la bulle
         float dx = fishCenterX - bubbleCenterX;
         float dy = fishCenterY - bubbleCenterY;
         float distance = (float) Math.sqrt(dx * dx + dy * dy);
 
-        // rayon de la bulle
+        //calcul du rayon de la bulle
         float bubbleRadius = bubbleSprite.getWidth() / 2f;
 
         // déplace le poisson uniquement si le jeu a commencé
@@ -119,22 +130,25 @@ public class Minijeu2 implements Minijeu {
         } else {
             this.directionY = 0;
         }
-        // Déplacement du poisson sur l'axe Y
+        //déplacement du poisson sur l'axe Y
         fishSprite.translateY(speedy * this.directionY);
 
-        // Affichage de la victoire si le poisson atteint la limite Y
+        //affichage de la victoire si le poisson atteint la limite Y
         if (fishSprite.getY() >= 190) {
             System.out.println("Vous avez gagné");
             this.State = 2;
         }
     }
 
+
+    //permet d'afficher les sprites du poisson et de la bulle
     @Override
     public void draw(PecheActiveScreen screen) {
         fishSprite.draw(screen.jeu.batch);
         bubbleSprite.draw(screen.jeu.batch);
     }
 
+    //permet de récupérer l'état du mini-jeu depuis la méthode appelante
     @Override
     public int getState() {
         return State;
